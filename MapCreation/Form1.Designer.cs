67672714e36de50bc65a2633700f073c17237687
Klapeyron <nikitoslav-razdol@ya.ru>
2018-04-10 18:48:06 +0300

    Initial

---
+++ MapCreation/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace MapCreation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 29);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(700, 700);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pictureBox2.Location = new System.Drawing.Point(718, 29);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(141, 141);
+            this.pictureBox2.TabIndex = 11;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pictureBox3.Location = new System.Drawing.Point(718, 189);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(141, 141);
+            this.pictureBox3.TabIndex = 12;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pictureBox4.Location = new System.Drawing.Point(865, 29);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(282, 282);
+            this.pictureBox4.TabIndex = 13;
+            this.pictureBox4.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Precise map";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.label2.Location = new System.Drawing.Point(818, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Scan 0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.label3.Location = new System.Drawing.Point(818, 173);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Scan 1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.label4.Location = new System.Drawing.Point(1086, 13);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 13);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Crosslinked";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(784, 336);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "Closslink";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(1454, 873);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.PictureBox pictureBox1;
+        public System.Windows.Forms.PictureBox pictureBox2;
+        public System.Windows.Forms.PictureBox pictureBox3;
+        public System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ MapCreation/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+using System;
+
+namespace MapCreation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.MouseDown += pictureBox_MouseDown;
+
+            preciseMap = new PixelMap("C:\\Adocuments\\Library\\Clapeyron_ind\\task6 map creation\\PreciseMap1.png");
+            pictureBox1.Image = preciseMap.GetBitmap();
+        }
+
+        private PixelMap preciseMap;
+        private PixelMap scan0;
+        private PixelMap scan1;
+
+        private const ushort n_phi = 360;
+        private double step = 2 * Math.PI / n_phi;
+        private ushort[] rByPhi0 = new ushort[n_phi];
+        private int X0, Y0; //0 scan centre
+        private ushort[] rByPhi1 = new ushort[n_phi];
+        private int X1, Y1; //real centre
+        private int X2, Y2; //supposed centre
+
+        private const ushort r_scan = 70;
+        private const ushort d_scan = 2 * r_scan + 1;
+        private const ushort r_scan1 = r_scan + 1;
+
+        private Color wallColor = Color.FromArgb(255, 255, 255);
+        private Color indoorColor = Color.FromArgb(0, 0, 0);
+        private Color startColor = Color.FromArgb(94, 255, 0);
+        private Color routeColor = Color.FromArgb(255, 51, 0);
+        private Color finishColor = Color.FromArgb(0, 222, 255);
+
+        private byte positionCounter = 0;
+
+        private void pictureBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            int X = e.X * preciseMap.Width / pictureBox1.Width;
+            int Y = e.Y * preciseMap.Height / pictureBox1.Height;
+
+            switch (positionCounter)
+            {
+                case 0:
+                    preciseMap[X0, Y0] = new Pixel(indoorColor);
+                    X0 = X;
+                    Y0 = Y;
+                    preciseMap[X0, Y0] = new Pixel(startColor);
+                    
+                    scan0 = new PixelMap(d_scan, d_scan, 0, 0, 0);
+                    rByPhi0 = getScanFromPreciseMap(X, Y, scan0);
+
+                    Bitmap map = preciseMap.GetBitmap();
+                    Pen pen = new Pen(wallColor);
+                    Graphics graphics = Graphics.FromImage(map);
+                    try
+                    {
+                        graphics.DrawEllipse(pen, 0,0,100,100);// X0 - r_scan-1, Y0 - r_scan-1, d_scan, d_scan);
+                        Console.WriteLine("HI");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Exception");
+                    }
+                    map.SetPixel(0,0,startColor);
+
+                    pictureBox1.Image = map;
+                    pictureBox2.Image = scan0.GetBitmap();
+                    positionCounter++;
+                    break;
+                case 1:
+                    preciseMap[X1, Y1] = new Pixel(indoorColor);
+                    X1 = X;
+                    Y1 = Y;
+                    preciseMap[X1, Y1] = new Pixel(finishColor);
+                    scan1 = new PixelMap(d_scan, d_scan, 0, 0, 0);
+                    rByPhi1 = getScanFromPreciseMap(X, Y, scan1);
+                    pictureBox3.Image = scan1.GetBitmap();
+                    positionCounter++;
+                    break;
+                case 2:
+                    preciseMap[X2, Y2] = new Pixel(indoorColor);
+                    X2 = X;
+                    Y2 = Y;
+                    preciseMap[X2, Y2] = new Pixel(routeColor);
+                    positionCounter = 0;
+                    break;
+            }
+            pictureBox1.Image = preciseMap.GetBitmap();
+        }
+
+        private ushort[] getScanFromPreciseMap(int X, int Y, PixelMap scanBmp)
+        {
+            ushort[] rByPhi = new ushort[n_phi];
+            int y = new int();
+            int x = new int();
+            bool flag;
+
+            for (int i = 0; i < n_phi; i++)
+            {
+                flag = false;
+                for (ushort r = 1; r < r_scan1; r++)
+                {
+                    x = (int)Math.Round(r * Math.Cos(i * step)) + X;
+                    y = (int)Math.Round(r * Math.Sin(i * step)) + Y;
+                    if (preciseMap[x, y].Color == wallColor)
+                    {
+                        rByPhi[i] = r;
+                        scanBmp[x - X + r_scan, y - Y + r_scan] = new Pixel(wallColor);
+                        flag = true;
+                        break;
+                    }
+                }
+                if (!flag)
+                    rByPhi[i] = 0;
+            }
+            return rByPhi;
+        }
+    }
+}
