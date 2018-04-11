0b521d9916561a5ce2c8dc97ce954fde5193dce7
Klapeyron <nikitoslav-razdol@ya.ru>
2018-04-11 19:59:13 +0300

    Almost ready UI for crosslink testing
    Except supposed center angle zone

--- MapCreation/Form1.cs
+++ MapCreation/Form1.cs
@@ -13,7 +13,7 @@ namespace MapCreation
             pictureBox1.MouseDown += pictureBox1_MouseDown;
             
             preciseMap = new PixelMap("C:\\Adocuments\\Library\\Clapeyron_ind\\task6 map creation\\PreciseMap1.png");
-            preciseIndoorMap = getPreciseIndoorMap(preciseMap);
+            preciseIndoorMap = getIndoorMap(preciseMap);
             pictureBox1.Image = preciseMap.GetBitmap();
         }
 
@@ -28,21 +28,22 @@ namespace MapCreation
         private const ushort r_robot = 5; //5+1 (потому что центральный px еще) = 6px = 25cm
         private const ushort r_scan = 70; //25cm*12=3m; 6px*12=72px ~ 70+1
         private const ushort l_max = 35; //1.5m
-        private const ushort sgm_lmax = 4; //4px = 16.5cm
-        private const double sgm_psi = 0.14; //rad (~20cm)
+        private const ushort sgm_lmax = 1; //3px = 12cm
+        private const int sgm_psi = 2;//in degrees: 2*3.14/180*1.5m=0.05m  //0.046; //3*0.046=0.14rad (~20cm)
     //    private const ushort sgm_r = 0; //D = f*h/px
 
         private double step = 2 * Math.PI / n_phi;
         private ushort[] rByPhi0 = new ushort[n_phi];
-        private int X0 = -1, Y0 = -1; //0 scan centre
+        private int X0 = -1, Y0 = -1; //0 scan center
         private ushort[] rByPhi1 = new ushort[n_phi];
-        private int X1 = -1, Y1 = -1; //real centre
-        private int X2 = -1, Y2 = -1; //supposed centre
+        private int X1 = -1, Y1 = -1; //real 1 scan center
+        private int X2 = -1, Y2 = -1; //supposed 1 scan center
 
         private const ushort r_robot1 = r_robot + 1;
         private const ushort d_robot = 2 * r_robot1;
         private const ushort r_scan1 = r_scan + 1;
         private const ushort d_scan = 2 * r_scan1;
+        private const ushort l_max2 = l_max  * l_max;
 
         private Color wallColor = Color.FromArgb(255, 255, 255);
         private Color indoorColor = Color.FromArgb(0, 0, 0);
@@ -51,10 +52,26 @@ namespace MapCreation
         private Color finishColor = Color.FromArgb(0, 222, 255);
 
         private byte positionCounter = 0;
-        
+        private double l_rl = 0; //[0,l_max]
+        private int l_rl_rounded = 0;
+        private int l_rl2 = 0; //[0,l_max^2]
+        private double psi_rl_rad = 0; //in radians
+        private double psi_rl_deg = 0; //in degrees
+        private double l_rlPlus3sgm;
+        private double l_rlMinus3sgm;
+
+
+        private double l_sp = 0; //[0,l_max]
+    //    private int l_rl_rounded = 0;
+        private int l_sp2 = 0; //[0,l_max^2]
+        private double psi_sp_rad = 0; //in radians
+    //    private double psi_rl_deg = 0; //in degrees
+
         /// <summary>
         /// Обработчик кликов на pictureBox1 (где отрисована preciseMap).
-        /// Первый клик устанавливает центра scan0, второй клик устанавливает центр scan1, третий клик устанавливает supposed центр scan1.
+        /// Первый клик устанавливает центр нулевого скана
+        /// Второй клик устанавливает центр первого скана: расстояние от центра scan1 до центра scan0 должно быть меньше l_max
+        /// Третий клик устанавливает supposed центр скана 1: может быть установлен только в зоне погрешности скана 1
         /// Все эти центры могут находиться только в indoor-среде.
         /// </summary>
         /// <param name="sender"></param>
@@ -65,7 +82,10 @@ namespace MapCreation
             int Y = e.Y * preciseMap.Height / pictureBox1.Height;
             if (preciseIndoorMap[X, Y].Color == indoorColor)
             {
-
+                Bitmap preciseMapBmp = preciseMap.GetBitmap();
+                Pen pen;
+                SolidBrush brush;
+                Graphics graphics = Graphics.FromImage(preciseMapBmp);
                 switch (positionCounter)
                 {
                     case 0:
@@ -77,42 +97,71 @@ namespace MapCreation
                         positionCounter++;
                         break;
                     case 1:
-                        X1 = X;
-                        Y1 = Y;
-                        scan1 = new PixelMap(d_scan, d_scan, 0, 0, 0);
-                        rByPhi1 = getScanFromPreciseMap(X, Y, scan1);
-                        pictureBox3.Image = scan1.GetBitmap();
-                        positionCounter++;
+                        l_rl2 = getSquaredDistance(X0, Y0, X, Y);
+                        l_rl = Math.Pow(l_rl2,0.5);
+                        l_rl_rounded = (int)Math.Round(l_rl);
+                        psi_rl_rad = Math.Atan2(Y-Y0,X-X0);
+                        psi_rl_deg = psi_rl_rad*180/Math.PI;
+                        Console.WriteLine(psi_rl_deg);
+                        if (l_rl2<=l_max2) {
+                            X1 = X;
+                            Y1 = Y;
+                            scan1 = new PixelMap(d_scan, d_scan, 0, 0, 0);
+                            rByPhi1 = getScanFromPreciseMap(X, Y, scan1);
+                            pictureBox3.Image = scan1.GetBitmap();
+                            positionCounter++;
+
+                            //отрисовать зону погрешности, в которую попадает supposed центр
+                            pen = new Pen(routeColor);
+                            //  graphics.DrawEllipse(pen, X0 - l_rl_rounded, Y0 - l_rl_rounded, 2 * l_rl_rounded, 2 * l_rl_rounded);
+                            double sgm_lrl = l_rl * sgm_lmax / l_max; //вычисляем погрешность передвижения, считая зависимость погрешности от пройденного расстояния линейной
+                            l_rlPlus3sgm = l_rl + 3 * sgm_lrl;
+                            l_rlMinus3sgm = l_rl - 3 * sgm_lrl;
+                            int lplus3sgmInt = (int)Math.Round(l_rlPlus3sgm);
+                            int lminus3sgmInt = (int)Math.Round(l_rlMinus3sgm);
+                            brush = new SolidBrush(routeColor);
+                            graphics.FillPie(brush, X0 - lplus3sgmInt - 1, Y0 - lplus3sgmInt - 1, 2 * lplus3sgmInt + 2, 2 * lplus3sgmInt + 2, (float)psi_rl_deg - 3 * sgm_psi, 6 * sgm_psi);
+                            brush = new SolidBrush(indoorColor);
+                            graphics.FillPie(brush, X0 - lminus3sgmInt, Y0 - lminus3sgmInt, 2 * lminus3sgmInt, 2 * lminus3sgmInt, (float)psi_rl_deg - 3 * sgm_psi, 6 * sgm_psi);
+                        }
                         break;
                     case 2:
-                        X2 = X;
-                        Y2 = Y;
-                        positionCounter = 0;
+                        //пока простая проверка: ровный разброс по углу и по длине
+                        l_sp2 = getSquaredDistance(X0, Y0, X, Y);
+                        l_sp = Math.Pow(l_sp2, 0.5);
+                        psi_sp_rad = Math.Atan2(Y - Y0, X - X0);
+                        if (((l_sp>=l_rlMinus3sgm)&&(l_sp<=l_rlPlus3sgm)))//&&((psi_sp_rad)&&())) {
+                        {   X2 = X;
+                            Y2 = Y;
+                            positionCounter = 0;
+                        }
                         break;
                 }
                 //отрисовать все три центра
                 //отрисовать радиусы для scan0 и scan1
-                Bitmap map = preciseMap.GetBitmap();
-                Pen pen;
-                Graphics graphics = Graphics.FromImage(map);
                 try
                 {
                     if ((X0 >= 0) && (Y0 >= 0))
                     {
-                        map.SetPixel(X0, Y0, startColor);
+                        preciseMapBmp.SetPixel(X0, Y0, startColor);
                         pen = new Pen(startColor);
                         graphics.DrawEllipse(pen, X0 - r_scan1, Y0 - r_scan1, d_scan, d_scan);
+                        graphics.DrawEllipse(pen, X0 - r_robot1, Y0 - r_robot1, d_robot, d_robot);
                     }
                     if ((X1 >= 0) && (Y1 >= 0))
                     {
-                        map.SetPixel(X1, Y1, finishColor);
+                        preciseMapBmp.SetPixel(X1, Y1, finishColor);
                         pen = new Pen(finishColor);
                         graphics.DrawEllipse(pen, X1 - r_scan1, Y1 - r_scan1, d_scan, d_scan);
+                        graphics.DrawEllipse(pen, X1 - r_robot1, Y1 - r_robot1, d_robot, d_robot);
                     }
-                    if ((X2 >= 0) && (Y2 >= 0)) map.SetPixel(X2, Y2, routeColor);
+                    if ((X2 >= 0) && (Y2 >= 0))
+                    {
+                        preciseMapBmp.SetPixel(X2, Y2, routeColor);
+                    }
                 }
                 catch (Exception ex) { }
-                pictureBox1.Image = map;
+                pictureBox1.Image = preciseMapBmp;
             }
         }
 
@@ -151,16 +200,21 @@ namespace MapCreation
             return rByPhi;
         }
 
-        private PixelMap getPreciseIndoorMap(PixelMap preciseMap)
+        /// <summary>
+        /// Возвращает карту indoor-среды для заданной карты.
+        /// </summary>
+        /// <param name="map"></param>
+        /// <returns></returns>
+        private PixelMap getIndoorMap(PixelMap map)
         {
-            Bitmap preciseIndoorMapBmp = preciseMap.GetBitmap();
+            Bitmap preciseIndoorMapBmp = map.GetBitmap();
             SolidBrush brush = new SolidBrush(wallColor);
             Graphics graphics = Graphics.FromImage(preciseIndoorMapBmp);
-            for (int i = 0; i < preciseMap.Width; i++)
+            for (int i = 0; i < map.Width; i++)
             {
-                for (int j = 0; j < preciseMap.Height; j++)
+                for (int j = 0; j < map.Height; j++)
                 {
-                    if (preciseMap[i,j].Color == wallColor)
+                    if (map[i,j].Color == wallColor)
                     {
                         try
                         {
@@ -173,6 +227,25 @@ namespace MapCreation
         //    preciseIndoorMapBmp.Save("C:\\Adocuments\\Library\\Clapeyron_ind\\task6 map creation\\PreciseIndoorMap1.png");
             PixelMap preciseIndoorMap = new PixelMap(preciseIndoorMapBmp);
             return preciseIndoorMap;
+        }
+
+        /// <summary>
+        /// Возвращает квадрат расстояния между точками.
+        /// </summary>
+        /// <param name="x1"></param>
+        /// <param name="y1"></param>
+        /// <param name="x2"></param>
+        /// <param name="y2"></param>
+        /// <returns></returns>
+        private int getSquaredDistance(int x1,int y1,int x2,int y2)
+        {
+            return ((x1-x2)*(x1-x2)+ (y1 - y2) * (y1 - y2));
+        }
+
+        private int getSigmaL(int l)
+        {
+
+            return 0;
         }
     }
 }
